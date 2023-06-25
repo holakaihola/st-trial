3e52cb57ad305e98829ce207771bb7a815fb7b8a
holakaihola <102952254+holakaihola@users.noreply.github.com>
2023-06-25 23:10:05 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
 # st.markdown("<h4 style='color: #0066cc'>Estimate Future Price of Your Ideal Home</h4>", unsafe_allow_html=True)
 
@@ -56,6 +57,11 @@ st.markdown("<h4 style='color: #0066cc'>Select the variables to use in clusterin
 
 selected_columns = st.multiselect("", column_names, default=default_columns)
 
+# Validate the number of selected columns
+if len(selected_columns) < 2:
+    st.error("Please select at least two columns.")
+    st.stop()
+    
 # # Apply a theme to change the color of the multiselect widget
 # st.markdown(
 #     """
@@ -93,24 +99,46 @@ X_scaled = scaler.fit_transform(selected_data)
 kmeans = KMeans(n_clusters=num_clusters)
 kmeans.fit(X_scaled)
 
-# Get the cluster labels
-labels_kmeans = kmeans.labels_
-data['labels_kmeans'] = labels_kmeans
+# # Get the cluster labels
+# labels_kmeans = kmeans.labels_
+# data['labels_kmeans'] = labels_kmeans
 
-# Finding the final centroids
-centroids = kmeans.cluster_centers_
-# Evaluating the quality of clusters
-s = metrics.silhouette_score(X_scaled, labels_kmeans, metric='euclidean')
-print(f"Silhouette Coefficient for the Dataset Clusters: {s:.2f}")
+# # Finding the final centroids
+# centroids = kmeans.cluster_centers_
+# # Evaluating the quality of clusters
+# s = metrics.silhouette_score(X_scaled, labels_kmeans, metric='euclidean')
+# print(f"Silhouette Coefficient for the Dataset Clusters: {s:.2f}")
 
-# Plot the clusters
-# , c=colors
-fig, ax = plt.subplots()
-ax.scatter(X_scaled[:, 0], X_scaled[:, 1], c=labels_kmeans, cmap='viridis')
-ax.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], marker='x', color='red', s=100)
-ax.set_xlabel('Feature 1')
-ax.set_ylabel('Feature 2')
-ax.set_title('K-means assigned Clustering on original df')
+# # Plot the clusters
+# # , c=colors
+# fig, ax = plt.subplots()
+# ax.scatter(X_scaled[:, 0], X_scaled[:, 1], c=labels_kmeans, cmap='viridis')
+# ax.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], marker='x', color='red', s=100)
+# ax.set_xlabel('Feature 1')
+# ax.set_ylabel('Feature 2')
+# ax.set_title('K-means assigned Clustering on original df')
 
-# Pass the figure object to st.pyplot()
-st.pyplot(fig)
+# # Pass the figure object to st.pyplot()
+# st.pyplot(fig)
+
+# Perform dimensionality reduction using PCA
+pca = PCA(n_components=2)
+pca_data = pca.fit_transform(selected_data)
+
+# Add the cluster labels to the data
+cluster_labels = kmeans.labels_
+pca_data_with_clusters = pd.DataFrame(pca_data, columns=['Component 1', 'Component 2'])
+pca_data_with_clusters['Cluster'] = cluster_labels
+
+# Create a scatter plot of the data points with colors representing the clusters
+plt.figure(figsize=(8, 6))
+for cluster in range(num_clusters):
+    cluster_data = pca_data_with_clusters[pca_data_with_clusters['Cluster'] == cluster]
+    plt.scatter(cluster_data['Component 1'], cluster_data['Component 2'], label=f'Cluster {cluster}')
+plt.xlabel('Component 1')
+plt.ylabel('Component 2')
+plt.title('K-means Clustering (PCA)')
+plt.legend()
+
+# Display the plot using Streamlit
+st.pyplot(plt)
