b4c0a2255ee31eed11ecd04b4b1822f87b06ecab
holakaihola <102952254+holakaihola@users.noreply.github.com>
2023-06-25 23:02:19 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,48 +15,21 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from sklearn.preprocessing import StandardScaler
 
-st.markdown("<h4 style='color: #0066cc'>Estimate Future Price of Your Ideal Home</h4>", unsafe_allow_html=True)
+# st.markdown("<h4 style='color: #0066cc'>Estimate Future Price of Your Ideal Home</h4>", unsafe_allow_html=True)
 
 st.markdown(
     """
     <div style='background-color: #0066cc; padding: 10px'>
-        <h2 style='color: white;text-align: center;'>Forecasting Flat Prices</h2>
+        <h2 style='color: white;text-align: center;'>
+        k-means clustering to predict CO2 emissions</h2>
     </div>
     """,
     unsafe_allow_html=True
 )
 
-st.markdown('''
-This is a dashboard showing the *forecasted prices* of different types of HDB :house: s
-''')
-
-
-# TOWNS = ["ANG MO KIO", "BEDOK", "BISHAN", "BUKIT BATOK", "BUKIT MERAH", "BUKIT PANJANG", "BUKIT TIMAH", "CENTRAL AREA","CHOA CHU KANG", "CLEMENTI", "GEYLANG", "HOUGANG", "JURONG EAST", "JURONG WEST", "KALLANG/WHAMPOA", "MARINE PARADE", "PASIR RIS", "PUNGGOL", "QUEENSTOWN", "SEMBAWANG", "SENGKANG", "SERANGOON", "TAMPINES", "TOA PAYOH", "WOODLANDS", "YISHUN"]
-# FLAT_TYPES = ['1 ROOM', '2 ROOM', '3 ROOM', '4 ROOM', '5 ROOM', 'EXECUTIVE', 'MULTI-GENERATION']
-# EXCLUSIONS = {
-#     "ANG MO KIO, 1 ROOM", "BEDOK, 1 ROOM", "BISHAN, 1 ROOM", "BUKIT BATOK, 1 ROOM", "BUKIT PANJANG, 1 ROOM", "BUKIT TIMAH, 1 ROOM", "CENTRAL AREA, 1 ROOM", "CHOA CHU KANG, 1 ROOM", "CLEMENTI, 1 ROOM", "GEYLANG, 1 ROOM", "HOUGANG, 1 ROOM", "JURONG EAST, 1 ROOM", "JURONG WEST, 1 ROOM", "KALLANG/WHAMPOA, 1 ROOM", "MARINE PARADE, 1 ROOM", "PASIR RIS, 1 ROOM", "PUNGGOL, 1 ROOM", "QUEENSTOWN, 1 ROOM", "SEMBAWANG, 1 ROOM", "SENGKANG, 1 ROOM", "SERANGOON, 1 ROOM", "TAMPINES, 1 ROOM", "TOA PAYOH, 1 ROOM", "WOODLANDS, 1 ROOM", "YISHUN, 1 ROOM",
-#     "BISHAN, 2 ROOM", "BUKIT BATOK, 2 ROOM", "BUKIT TIMAH, 2 ROOM", "MARINE PARADE, 2 ROOM",
-#     "BUKIT MERAH, EXECUTIVE", "CENTRAL AREA, EXECUTIVE", "MARINE PARADE, EXECUTIVE",
-#     "ANG MO KIO, MULTI-GENERATION", "BEDOK, MULTI-GENERATION", "BUKIT BATOK, MULTI-GENERATION", "BUKIT MERAH, MULTI-GENERATION", "BUKIT PANJANG, MULTI-GENERATION", "BUKIT TIMAH, MULTI-GENERATION", "CENTRAL AREA, MULTI-GENERATION","CHOA CHU KANG, MULTI-GENERATION", "CLEMENTI, MULTI-GENERATION", "GEYLANG, MULTI-GENERATION", "HOUGANG, MULTI-GENERATION", "JURONG EAST, MULTI-GENERATION", "JURONG WEST, MULTI-GENERATION", "KALLANG/WHAMPOA, MULTI-GENERATION", "MARINE PARADE, MULTI-GENERATION", "PASIR RIS, MULTI-GENERATION", "PUNGGOL, MULTI-GENERATION", "QUEENSTOWN, MULTI-GENERATION", "SEMBAWANG, MULTI-GENERATION", "SENGKANG, MULTI-GENERATION", "SERANGOON, MULTI-GENERATION", "TOA PAYOH, MULTI-GENERATION", "WOODLANDS, MULTI-GENERATION"
-#     }
-
-# town_flat_types = [t+', '+ft for ft in FLAT_TYPES for t in TOWNS if t+', '+ft not in EXCLUSIONS]
-# defaults = town_flat_types[:2]
-
-# options = st.multiselect(
-#     'Select Town and Flat Type',
-#     town_flat_types,
-#     defaults,
-#     max_selections=4)
-
-
-# Set the default number of clusters
-default_clusters = 3
-
-# Display the slider for selecting the number of clusters
-num_clusters = st.sidebar.slider("Number of Clusters", min_value=2, max_value=10, value=default_clusters)
-
-
+# st.markdown('''
+# This is a dashboard showing the *forecasted prices* of different types of HDB :house: s
+# ''')
 
 ### READ DATA ###
 file_location = "USData.csv"
@@ -72,14 +45,49 @@ data['GDP'] = data['GDP'].astype(float)
 
 # col1, mid, col2 = st.columns([2,0.5,2])
 
+# Get the column names of the DataFrame
+column_names = data.columns.tolist()
+
+# Set the default selected columns
+default_columns = ['Energy', 'GDP', 'CO2']
+
+# Display the multi-select widget for selecting columns
+st.markdown("<h4 style='color: #0066cc'>Select the variables to use in clustering</h4>", unsafe_allow_html=True)
+
+selected_columns = st.multiselect("", column_names, default=default_columns)
+
+# # Apply a theme to change the color of the multiselect widget
+# st.markdown(
+#     """
+#     <style>
+#     [data-baseweb="select"] { color: black !important; }
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+# )
+
+
+# Subset the data with the selected columns
+selected_data = data[selected_columns]
+
+# Cast the selected data columns as float
+selected_data = selected_data.astype(float)
+
+# Set the default number of clusters
+default_clusters = 3
+
+# Display the slider for selecting the number of clusters
+
+st.markdown("<h4 style='color: #0066cc'>Select the Number of Clusters</h4>", unsafe_allow_html=True)
+
+num_clusters = st.slider("", min_value=2, max_value=10, value=default_clusters)
 
 # Extract the 3 numerical variables
 X = data.iloc[:, 1:4].values
 
 #Scale the data 
 scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
-
+X_scaled = scaler.fit_transform(selected_data)
 
 # Perform K-means clustering
 kmeans = KMeans(n_clusters=num_clusters)
