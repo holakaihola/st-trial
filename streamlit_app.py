7ecbea448c070a5661e6ef0b99e3a7bf37442078
holakaihola <102952254+holakaihola@users.noreply.github.com>
2023-06-28 18:10:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,21 +16,16 @@ from sklearn.metrics import silhouette_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
-# st.markdown("<h4 style='color: #0066cc'>Estimate Future Price of Your Ideal Home</h4>", unsafe_allow_html=True)
 
 st.markdown(
     """
     <div style='background-color: #0066cc; padding: 10px'>
         <h2 style='color: white;text-align: center;'>
-        k-means clustering to predict CO2 emissions</h2>
+        k-means clustering to predict <br>CO2 emissions in 3 steps:</h2>
     </div>
     """,
     unsafe_allow_html=True
 )
-
-# st.markdown('''
-# This is a dashboard showing the *forecasted prices* of different types of HDB :house: s
-# ''')
 
 ### READ DATA ###
 file_location = "USData.csv"
@@ -38,11 +33,11 @@ data = pd.read_csv(file_location, index_col=0)
 
 data = data.rename(columns = {'Energy use (kg of oil equivalent per capita)':'Energy',\
                               'GDP per capita (current US$)':'GDP',\
-                                  'CO2 emissions NET':'CO2'})
+                                  'CO2 emissions NET':'CO2 Emissions'})
 
-data = data.iloc[:56]    
-data['Energy'] = data['Energy'].astype(float)
-data['GDP'] = data['GDP'].astype(float)
+data = data.iloc[1:56].astype(float)
+# data['Energy'] = data['Energy'].astype(float)
+# data['GDP'] = data['GDP'].astype(float)
 
 # col1, mid, col2 = st.columns([2,0.5,2])
 
@@ -50,10 +45,10 @@ data['GDP'] = data['GDP'].astype(float)
 column_names = data.columns.tolist()
 
 # Set the default selected columns
-default_columns = ['Energy', 'GDP', 'CO2']
+default_columns = ['Energy', 'GDP', 'CO2 Emissions']
 
 # Display the multi-select widget for selecting columns
-st.markdown("<h4 style='color: #0066cc'>Select the variables to use in clustering</h4>", unsafe_allow_html=True)
+st.markdown("<h4 style='color: #0066cc'>Step 1: Select the variables to use in clustering</h4>", unsafe_allow_html=True)
 
 selected_columns = st.multiselect("", column_names, default=default_columns)
 
@@ -77,19 +72,21 @@ if len(selected_columns) < 2:
 selected_data = data[selected_columns]
 
 # Cast the selected data columns as float
-selected_data = selected_data.astype(float)
+# selected_data = selected_data.astype(float)
 
 # Set the default number of clusters
 default_clusters = 3
 
 # Display the slider for selecting the number of clusters
 
-st.markdown("<h4 style='color: #0066cc'>Select the Number of Clusters</h4>", unsafe_allow_html=True)
+st.markdown("<h4 style='color: #0066cc'>Step 2: Select the Number of Clusters</h4>", unsafe_allow_html=True)
 
 num_clusters = st.slider("", min_value=2, max_value=10, value=default_clusters)
 
-# Extract the 3 numerical variables
-X = data.iloc[:, 1:4].values
+###########################################
+######## Scaling and Kmeans ###############
+###########################################
+st.markdown(f"<h4 style='color: #0066cc'>Step 3: Evaluate Outputs for {num_clusters} clusters</h4>", unsafe_allow_html=True)
 
 #Scale the data 
 scaler = StandardScaler()
@@ -99,27 +96,25 @@ X_scaled = scaler.fit_transform(selected_data)
 kmeans = KMeans(n_clusters=num_clusters)
 kmeans.fit(X_scaled)
 
-# # Get the cluster labels
-# labels_kmeans = kmeans.labels_
-# data['labels_kmeans'] = labels_kmeans
+# Add the cluster labels to the data
+data['Cluster'] = kmeans.labels_
 
-# # Finding the final centroids
-# centroids = kmeans.cluster_centers_
-# # Evaluating the quality of clusters
-# s = metrics.silhouette_score(X_scaled, labels_kmeans, metric='euclidean')
-# print(f"Silhouette Coefficient for the Dataset Clusters: {s:.2f}")
-
-# # Plot the clusters
-# # , c=colors
-# fig, ax = plt.subplots()
-# ax.scatter(X_scaled[:, 0], X_scaled[:, 1], c=labels_kmeans, cmap='viridis')
-# ax.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], marker='x', color='red', s=100)
-# ax.set_xlabel('Feature 1')
-# ax.set_ylabel('Feature 2')
-# ax.set_title('K-means assigned Clustering on original df')
-
-# # Pass the figure object to st.pyplot()
-# st.pyplot(fig)
+###########################################
+######## PCA ##############################
+###########################################
+# Define a custom color mapping for up to 10 clusters
+color_mapping = {
+    0: 'tab:blue',
+    1: 'tab:orange',
+    2: 'tab:green',
+    3: 'tab:red',
+    4: 'tab:purple',
+    5: 'tab:brown',
+    6: 'tab:pink',
+    7: 'tab:gray',
+    8: 'tab:olive',
+    9: 'tab:cyan'
+}
 
 # Perform dimensionality reduction using PCA
 pca = PCA(n_components=2)
@@ -134,11 +129,106 @@ pca_data_with_clusters['Cluster'] = cluster_labels
 plt.figure(figsize=(8, 6))
 for cluster in range(num_clusters):
     cluster_data = pca_data_with_clusters[pca_data_with_clusters['Cluster'] == cluster]
-    plt.scatter(cluster_data['Component 1'], cluster_data['Component 2'], label=f'Cluster {cluster}')
+    plt.scatter(cluster_data['Component 1'], cluster_data['Component 2'], label=f'Cluster {cluster + 1}')
 plt.xlabel('Component 1')
 plt.ylabel('Component 2')
-plt.title('K-means Clustering (PCA)')
+plt.title('K-means Clustering PCA Visualization')
 plt.legend()
 
 # Display the plot using Streamlit
 st.pyplot(plt)
+
+###################################################
+##### Performance metrics ####
+###################################################
+####### Section Title #######
+# st.markdown(
+#     """
+#     <div style='background-color: #0066cc; padding: 10px'>
+#         <h3 style='color: white;text-align: center;'>
+#         k-means Cluster Performance Metrics</h3>
+#     </div>
+#     """,
+#     unsafe_allow_html=True
+# )
+
+from sklearn.metrics import silhouette_score, calinski_harabasz_score, davies_bouldin_score
+
+silhouette_avg_kmeans = silhouette_score(X_scaled, cluster_labels)
+calinski_score_kmeans = calinski_harabasz_score(X_scaled, cluster_labels)
+davies_bouldin_kmeans = davies_bouldin_score(X_scaled, cluster_labels)
+
+# Set Streamlit theme color
+st.markdown(f'<style>body{{background-color: #0066cc;}}</style>', unsafe_allow_html=True)
+
+# Display scores as a summary table
+st.markdown("<h3 style='color: #0066cc;'>k-means Cluster Performance Metrics</h3>", unsafe_allow_html=True)
+st.subheader("**Silhouette Score:**")
+st.markdown(f"<p style='color:#0066cc; font-size: 24px;'>{round(silhouette_avg_kmeans, 2)}</p>",unsafe_allow_html=True)
+
+  # st.subheader("**Your monthly payment:**\n" + "$" + (monthly_payment, format='{:,d}'))
+st.subheader("**Calinski-Harabasz Score:**")
+st.markdown(f"<p style='color:#0066cc; font-size: 24px;'>{round(calinski_score_kmeans, 2)}</p>", unsafe_allow_html=True)
+
+st.subheader("**Davies-Bouldin Score**")
+st.markdown(f"<p style='color:#0066cc; font-size: 24px;'>{round(davies_bouldin_kmeans, 2)}</p>", unsafe_allow_html=True)
+###################################################
+##### Time Series plots for selected variables ####
+###################################################
+####### Section Title #######
+
+st.markdown(
+    f"""
+    <div style='background-color: #5893d4; padding: 10px'>
+        <h2 style='color: white;text-align: center;'>
+        k-means {num_clusters} clusters overlayed on original Data</h2>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+
+
+from matplotlib.patches import Patch
+
+
+# Define a dictionary mapping full column names to shortened versions
+title_mapping = {
+    'Energy': 'Energy',
+    'GDP': 'GDP',
+    'CO2 Emissions': 'CO2 Emissions',
+    'Merchandise trade (% of GDP)': 'Merchandise trade',
+    'Population density (people per sq. km of land area)': 'Population density',
+    'Population growth (annual %)': 'Population growth',
+    'Urban population growth (annual %)': 'Urban population growth',
+    'Crop production index (2014-2016 = 100)': 'Crop production index',
+    'Labor force participation rate, total (% of total population ages 15+) (national estimate)': 'Labor force participation rate',
+    'Merchandise exports (current US$)': 'Merchandise exports',
+    'Urban population': 'Urban population',
+    'Cluster': 'Cluster'
+}
+
+fig, axs = plt.subplots(len(selected_columns), 1, figsize=(10, 10), sharex=True)
+
+for i, column in enumerate(selected_columns):
+    axs[i].plot(data.index, data[column], color='gray')
+    scatter = axs[i].scatter(data.index, data[column], c=data['Cluster'].map(color_mapping))
+    axs[i].set_title(title_mapping[column])
+
+# Get the unique cluster labels present in data['Cluster']
+unique_clusters = data['Cluster'].unique()
+
+# Create custom legend for the clusters present
+legend_elements = []
+for cluster in unique_clusters:
+    legend_elements.append(Patch(facecolor=color_mapping[cluster], label=f'Cluster {cluster + 1}'))
+
+plt.xlabel("Year")
+# plt.suptitle('Selected Columns')
+plt.tight_layout()
+
+# Add the legend to the plot
+plt.legend(handles=legend_elements)
+
+st.pyplot(plt)
+
